88866ebcddff310c11f44636d52b3d365dc19527
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-12-17 19:45:17 +0800

    iplbootmessage-phase-2

--- ipl.asm
+++ ipl.asm
@@ -1,46 +1,49 @@
-[bits 16]           ; tell assembler that working in real mode(16 bit mode)
-[org 0x7c00]        ; organize from 0x7C00 memory location where BIOS will load us
+[bits 16]           ; 16-bit mode
+[org 0x7c00]        ; 0x7c00 is the BIOS load memory location
 
 
-start:              ; start label from where our code starts
-
-    xor ax,ax           ; set ax register to 0
-    mov ds,ax           ; set data segment(ds) to 0
-    mov es,ax           ; set extra segment(es) to 0
-    mov bx,0x8000
+start:
 
 
-    ;mov si, hello_world              ; point hello_world to source index
-    ;call print_string                ; call print different color string function
-    call DispStr
-    jmp $ ; and loop forever
+    xor ax,ax           ; set ax to 0: xor: output 1 only when different bits --- 0 xor 1 or 1 xor 0
+    mov ds,ax           ; set ds to 0
+    mov es,ax           ; set es to 0
+    mov bx,0x8000       ; memory location above 0x7c00
+    
+    call clear_screen   ; clear pre-load info
+
+
+    mov si, hello_world              ; point hello_world to si
+    call print_string                ; call print different color string function
 
 
 
-    hello_world db 'Hello ZiZoyaOS!',13,0
-
-
-DispStr:
-    mov ah,9
-    mov al,"hwiqewopoeqwqeoq"
-    mov bh,0
-    mov bl,4
-    mov cx,1
-    int 10h
-
+    hello_world db  '------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------Hello, ZizoyaOS is booting...-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------',0
+    ;define/declare bytes for hello_world
 
 print_string:
-    mov ah, 0x0E  ; Write instruction for int 0x10
+    mov ah, 0x0E            ; Write #0x0E instruction for int 0x10
 
 .repeat_next_char:
     lodsb                        ; Load byte at address SI into AL
     cmp al, 0                    ; cmp al with end of string
-    je .done_print               ; je: jump if equal
-    int 0x10                     ; else: print character
-    jmp .repeat_next_char        ; jmp to .repeat_next_char if not 0
+    je .done_print               ; je: jump to .done_print if equal
+    int 0x10                     ; else: call BIOS function 0x10 to print char in al
+    jmp .repeat_next_char        ; print recursively
 
 .done_print:
     ret
 
-    times (510 - ($ - $$)) db 0x00     ;fill 512 bytes
-    dw 0xAA55
+clear_screen:                ; function for clear pre-load info including BIOS info  
+    mov    ah, 0x06          ; Write #0x06 instruction for int 0x10 
+    mov    al, 0 
+    mov    ch, 0
+    mov    ch, 0    
+    mov    dh, 24 
+    mov    dl, 79
+    mov    bh, 0x07  
+    int 0x10  
+    ret  
+
+times (510 - ($ - $$)) db 0x00     ; fill up to 512 bytes
+dw 0xAA55                          ; boot-sector signature "magic number" 0xAA & 0x55
