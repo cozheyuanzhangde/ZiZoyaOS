99ecb77fdac3cb367d70f2d611e403eb6a3f65b9
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-12-19 21:24:46 +0800

    Create clear_screen.asm

---
+++ ipl-2-with-read-disk&print-hex/clear_screen.asm
@@ -0,0 +1,10 @@
+clear_screen:                ; function for clear pre-load info including BIOS info  
+    mov    ah, 0x06          ; Write #0x06 instruction for int 0x10 
+    mov    al, 0 
+    mov    ch, 0
+    mov    ch, 0    
+    mov    dh, 24 
+    mov    dl, 79
+    mov    bh, 0x07  
+    int 0x10                 ; call BIOS function 0x10 to print char in al
+    ret
